3532edc3866a6722352ecc14db5358460e8c4474
DavinciShah <shahrock911@gmail.com>
2026-04-15 23:46:16 +0530

    Add backend controllers for various functionalities

---
+++ backend/src/controllers/adminController.js
@@ -0,0 +1,9 @@
+// Controllers for admin dashboard
+
+class AdminController {
+    getDashboardData(req, res) {
+        // logic to get dashboard data
+    }
+}
+
+module.exports = new AdminController();
---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,9 @@
+// Controllers for products
+
+class ProductController {
+    getProducts(req, res) {
+        // logic to get products
+    }
+}
+
+module.exports = new ProductController();
