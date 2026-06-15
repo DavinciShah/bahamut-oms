77ff030832410c3fb4a2beaa3baafd46cbdbe03c
DavinciShah <shahrock911@gmail.com>
2026-06-16 00:49:23 +0530

    build: simplify MainActivity to standard BridgeActivity, resolving AdMob symbol compilation error

--- android/native/app/src/main/java/com/bahamut/oms/android/MainActivity.java
+++ android/native/app/src/main/java/com/bahamut/oms/android/MainActivity.java
@@ -1,14 +1,6 @@
 package com.bahamut.oms.android;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        MobileAds.initialize(this, initializationStatus -> {});
-    }
 }
